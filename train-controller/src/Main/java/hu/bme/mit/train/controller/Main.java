f6843e7256aee4ed99e1dfa7988ccb5c2d4d5f4d
Bödör Balázs <bodorbalazs44@gmail.com>
2021-03-08 09:35:45 +0100

    Speedlimit changed , main created

---
+++ train-controller/src/Main/java/hu/bme/mit/train/controller/Main.java
@@ -0,0 +1,9 @@
+package hu.bme.mit.train.controller;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the epic train!"); // Display the string.
+        TrainControllerImpl trainControl = new TrainControllerImpl();
+        trainControl.setSpeedLimit(222);
+    }
+}
